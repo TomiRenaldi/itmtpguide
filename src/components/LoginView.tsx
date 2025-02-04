15a69fc7239ae8ffc82a95598d6f0ab0fab2c806
TomiRenaldi <tomirenaldi2@gmail.com>
2025-02-04 11:15:50 +0700

    Desain ulang Login > Register

---
+++ src/components/LoginView.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function LoginView() {
+    return (
+        <form className="space-y-6">
+            <div>
+                <label htmlFor="" className="block text-sm font-medium text-gray-700 mb-1">
+                    Email Address
+                </label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
